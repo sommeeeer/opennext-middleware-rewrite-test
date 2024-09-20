f2cbbe7e6ee65648fe70c5b5238ea78d5f644fda
magnus <magnus@dahleide.com>
2024-09-20 12:40:41 +0200

    update headers

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/:path*', // Match all pages
+        headers: [
+          {
+            key: 'x-custom-header',
+            value: 'my custom header value',
+          },
+          {
+            key: 'x-another-custom-header',
+            value: 'my other custom header value',
+          },
+        ],
+      },
+    ];
+  },
+};
 export default nextConfig;
